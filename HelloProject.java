79181813eb3f1d3ab4f495a0fc37fa993176f34a
reshma naik <rnreshunaik@gmail.com>
2018-04-24 13:04:12 +0530

    added some new files

---
+++ HelloProject.java
@@ -0,0 +1,10 @@
+public class HelloProject {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i=0;i<10;i++){
+        System.out.println("hello world");
+        }
+    }
+    
+}
---
+++ additionjava.java
@@ -0,0 +1,9 @@
+public class additionjava {
+
+    public static void main(String[] args) {
+        int first = 3;
+        int second = 13;
+        System.out.println("FIRST=" + first + "SECOND=" + second);
+        }
+   }
+   
