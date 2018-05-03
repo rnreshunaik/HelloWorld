ff77481a01e7c8ec45c2d54173e041f4a15dffde
reshma naik <rnreshunaik@gmail.com>
2018-05-03 16:29:14 +0530

    all new recent files added

---
+++ Fabonacci.java
@@ -0,0 +1,19 @@
+public class Fabonacci{
+
+    public static void main(String[] args) {
+       int a=0,b=1,c;
+       System.out.println(a);
+       System.out.println(b);
+       c=a+b;
+       System.out.println(c);
+       while(c<89)
+               {
+                  a=b;
+                  b=c;
+                  c=a+b;
+                  System.out.println(c);
+               }
+    }
+    
+}
+
